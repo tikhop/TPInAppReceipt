a2964ac1e468f985715aef2e8b377e09822ec859
Pavel Tikhonenko <hi@tikhop.com>
2016-09-30 14:32:11 -0700

    decode all necessary values from receipt

--- TPInAppReceipt/Source/InAppReceipt.swift
+++ TPInAppReceipt/Source/InAppReceipt.swift
@@ -32,15 +32,14 @@ public enum InAppReceiptField: Int
 public struct InAppReceipt
 {
     public var bundleIdentifier: String
-    public let appVersion: String
-    public let originalAppVersion: String
-    public let purchases: [InAppPurchase]
-    public let expirationDate: Date
+    public var appVersion: String
+    public var originalAppVersion: String
+    public var purchases: [InAppPurchase]
+    public var expirationDate: String? = nil
     
-    public let quantity: Int
-    public let bundleIdentifierData: Data
-    public let opaqueValue: Data
-    public let receiptHash: Data
+    public var bundleIdentifierData: Data
+    public var opaqueValue: Data
+    public var receiptHash: Data
     
     public init(ans1Data: Data)
     {
@@ -48,10 +47,67 @@ public struct InAppReceipt
         appVersion = ""
         originalAppVersion = ""
         purchases = []
-        expirationDate = Date()
         bundleIdentifierData = Data()
         opaqueValue = Data()
         receiptHash = Data()
+        
+        ans1Data.enumerateASN1Attributes { (attributes) in
+            if let field = InAppReceiptField(rawValue: attributes.type)
+            {
+                let length = attributes.data.count
+                
+                var bytes = [UInt8](repeating:0, count: length)
+                attributes.data.copyBytes(to: &bytes, count: length)
+                
+                var ptr = UnsafePointer<UInt8>?(bytes)
+                
+                switch field
+                {
+                case .bundleIdentifier:
+                    bundleIdentifierData = Data(bytes: bytes, count: length)
+                    bundleIdentifier = asn1ReadUTF8String(&ptr, bytes.count)
+                case .appVersion:
+                    appVersion = asn1ReadUTF8String(&ptr, bytes.count)
+                case .opaqueValue:
+                    opaqueValue = Data(bytes: bytes, count: length)
+                case .receiptHash:
+                    receiptHash = Data(bytes: bytes, count: length)
+                case .inAppPurchaseReceipt:
+                    purchases.append(InAppPurchase(ans1Data: attributes.data))
+                case .originalAppVersion:
+                    originalAppVersion = asn1ReadUTF8String(&ptr, bytes.count)
+                case .expirationDate:
+                    let str = asn1ReadASCIIString(&ptr, bytes.count)
+                    expirationDate = str
+                default:
+                    print("attribute.type = \(attributes.type))")
+                }
+            }
+            
+            
+        }
+    }
+}
+
+public struct InAppPurchase
+{
+    public var quantity: Int
+    public var productIdentifier: String
+    public var transactionIdentifier: String
+    public var originalTransactionIdentifier: String
+    public var purchaseDate: String
+    public var originalPurchaseDate: String
+    public var subscriptionExpirationDate: String? = nil
+    public var cancellationDate: String? = nil
+    public var webOrderLineItemID: Int? = nil
+    
+    public init(ans1Data: Data)
+    {
+        originalTransactionIdentifier = ""
+        productIdentifier = ""
+        transactionIdentifier = ""
+        purchaseDate = ""
+        originalPurchaseDate = ""
         quantity = 0
         
         ans1Data.enumerateASN1Attributes { (attributes) in
@@ -66,9 +122,24 @@ public struct InAppReceipt
                 
                 switch field
                 {
-                case InAppReceiptField.bundleIdentifier:
-                    let str = asn1ReadUTF8String(&ptr, bytes.count)
-                    bundleIdentifier = str
+                case .quantity:
+                    quantity = asn1ReadInteger(&ptr, l: bytes.count)
+                case .productIdentifier:
+                    productIdentifier = asn1ReadUTF8String(&ptr, bytes.count)
+                case .transactionIdentifier:
+                    transactionIdentifier = asn1ReadUTF8String(&ptr, bytes.count)
+                case .purchaseDate:
+                    purchaseDate = asn1ReadASCIIString(&ptr, bytes.count)
+                case .originalTransactionIdentifier:
+                    originalTransactionIdentifier = asn1ReadUTF8String(&ptr, bytes.count)
+                case .originalPurchaseDate:
+                    originalPurchaseDate = asn1ReadUTF8String(&ptr, bytes.count)
+                case .expirationDate:
+                    subscriptionExpirationDate = asn1ReadUTF8String(&ptr, bytes.count)
+                case .cancellationDate:
+                    cancellationDate = asn1ReadUTF8String(&ptr, bytes.count)
+                case .webOrderLineItemID:
+                    webOrderLineItemID = asn1ReadInteger(&ptr, l: bytes.count)
                 default:
                     print("attribute.type = \(attributes.type))")
                 }
@@ -77,17 +148,4 @@ public struct InAppReceipt
             
         }
     }
-}
-
-public struct InAppPurchase
-{
-    public let quantity: Int
-    public let productIdentifier: String
-    public let transactionIdentifier: String
-    public let originalTransactionIdentifier: String
-    public let purchaseDate: Date
-    public let originalPurchaseDate: Date
-    public let subscriptionExpirationDate: Date
-    public let cancellationDate: Date
-    public let webOrderLineItemID: Int
 }
