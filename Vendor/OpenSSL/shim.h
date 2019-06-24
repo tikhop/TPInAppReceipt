2766e8fed96378aa32878bb7f15600089a36dccd
Pavel Tikhonenko <hi@tikhop.com>
2019-06-24 15:08:50 +0300

    keep working on removing openssl

---
+++ Vendor/OpenSSL/shim.h
@@ -0,0 +1,13 @@
+//
+//  shim.h
+//  TPInAppReceipt
+//
+//  Created by Pavel Tikhonenko on 24/06/2019.
+//  Copyright © 2019 Pavel Tikhonenko. All rights reserved.
+//
+
+#ifndef shim_h
+#define shim_h
+
+
+#endif /* shim_h */
