81f70cfd5039f414c927eb0bc5ece71903f45564
Axel <axelshaded@gmail.com>
2019-11-19 03:54:18 +0800

    Remove unused code
    Remove unused code

--- TPInAppReceipt/Source/Data+Extension.swift
+++ TPInAppReceipt/Source/Data+Extension.swift
@@ -7,47 +7,6 @@
 //
 
 import Foundation
-import CommonCrypto
-
-public typealias DigestAlgorithmClosure = (_ data: UnsafePointer<UInt8>, _ dataLength: UInt32) -> [UInt8]
-
-public enum DigestAlgorithm: CustomStringConvertible {
-    case sha1
-    
-    func progressClosure() -> DigestAlgorithmClosure {
-        var closure: DigestAlgorithmClosure?
-        
-        switch self {
-            case .sha1:
-            closure = {
-                var hash = [UInt8](repeating: 0, count: self.digestLength())
-                CC_SHA1($0, $1, &hash)
-                
-                return hash
-            }
-        }
-        
-        return closure!
-    }
-    
-    func digestLength() -> Int {
-        var result: CInt = 0
-        switch self {
-            case .sha1:
-            result = CC_SHA1_DIGEST_LENGTH
-        }
-        return Int(result)
-    }
-    
-    public var description: String {
-        get {
-            switch self {
-                case .sha1:
-                return "Digest.SHA1"
-            }
-        }
-    }
-}
     
 extension Data
 {
@@ -82,20 +41,4 @@ extension Data
         return bytesArray
     }
     
-    /// Digest data to an array of UInt8
-    public func digestBytes(_ algorithm:DigestAlgorithm)->[UInt8]{
-        let string = (self as NSData).bytes.bindMemory(to: UInt8.self, capacity: self.count)
-        let stringLength = UInt32(self.count)
-        
-        let closure = algorithm.progressClosure()
-        
-        let bytes = closure(string, stringLength)
-        return bytes
-    }
-    
-    /// Digest data with an algorithm
-    public func digestData(_ algorithm:DigestAlgorithm)->Data{
-        let bytes = self.digestBytes(algorithm)
-        return Data(bytes: UnsafePointer<UInt8>(bytes), count: bytes.count)
-    }
 }
