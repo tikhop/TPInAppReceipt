097aacafc8830cb1e7c20b3e23903365d29635fc
Pavel Tikhonenko <hi@tikhop.com>
2020-06-18 18:37:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "TPInAppReceipt",
+	platforms: [.macOS(.v10_11),
+				.iOS(.v9),
+				.tvOS(.v9),
+				.watchOS(.v2)],
     products: [
         .library(name: "TPInAppReceipt", targets: ["TPInAppReceipt"]),
     ],
