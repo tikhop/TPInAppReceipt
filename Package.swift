d72e478f552062cdcb09b851c3425158bff113cd
Pavel Tikhonenko <hi@tikhop.com>
2020-06-25 23:38:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,18 +2,9 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "TPInAppReceipt",
-	platforms: [.macOS(.v10_11),
-				.iOS(.v9),
-				.tvOS(.v9),
-				.watchOS(.v2)],
-    products: [
-        .library(name: "TPInAppReceipt", targets: ["TPInAppReceipt"]),
-    ],
-    targets: [
-        .target(
-            name: "TPInAppReceipt",
-			path: "TPInAppReceipt/Source")
-    ]
-)
+let package = Package(name: "TPInAppReceipt",
+					  platforms: [.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+					  products: [.library(name: "TPInAppReceipt", targets: ["TPInAppReceipt"])],
+					  targets: [.target(name: "TPInAppReceipt",
+										path: "TPInAppReceipt/Source",
+										resources: [.process("AppleIncRootCertificate.cer")])])
