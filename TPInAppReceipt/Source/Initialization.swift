51a85ca5dffbc65cb7685114216661db8acc6312
Pavel Tikhonenko <hi@tikhop.com>
2020-06-26 13:10:03 +0300

    Workaround for : Make it possible to use with pods and spm

--- TPInAppReceipt/Source/Initialization.swift
+++ TPInAppReceipt/Source/Initialization.swift
@@ -61,12 +61,10 @@ public extension Bundle
     
     class func lookUp(forResource name: String, ofType ext: String?) -> String?
     {
-		#if swift(>=5.3)
 		if let p = Bundle.module.path(forResource: name, ofType: ext)
 		{
 			return p
 		}
-		#endif
 		
         if let p = Bundle.main.path(forResource: name, ofType: ext)
         {
